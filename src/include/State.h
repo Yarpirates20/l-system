4049c15917ae13c4b63ce22ef949d8aa13d45ddb
Robert Samoraj <rsamoraj11@gmail.com>
2026-04-29 13:33:24 -0500

    Add State

---
+++ src/include/State.h
@@ -0,0 +1,11 @@
+#ifndef STATE_H
+#define STATE_H
+
+struct State
+{
+    float x;
+    float y;
+    float angle;
+};
+
+#endif // STATE_H
---
+++ src/include/StateEngine.h
@@ -0,0 +1,16 @@
+#ifndef STATEENGINE_H
+#define STATEENGINE_H
+
+#include "State.h"
+
+#include <stack>
+
+class StateEngine
+{
+    private:
+        std::stack<State> s;
+
+    public:
+};
+
+#endif // STATEENGINE_H
